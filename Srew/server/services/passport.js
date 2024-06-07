d11a7c36957203c1b7a91bfab7a4608d2c7a769e
Ryan Shamala <ryanshamala254@gmail.com>
2024-06-07 20:42:47 +0300

    rfurf

--- Srew/server/services/passport.js
+++ Srew/server/services/passport.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: process.env.GG_CLIENT_ID ?? "",
       clientSecret: process.env.GG_CLIENT_SECRET ?? "",
-      callbackURL: "/auth/google/callback",
+      callbackURL: "/api/auth/google/callback",
       scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
